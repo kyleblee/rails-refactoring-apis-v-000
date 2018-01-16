2e109d31c844907cdd60f7f68d0d646c143f3e96
kyleblee <hello.kylelee@gmail.com>
2018-01-15 22:18:09 -0500

    lab complete

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,12 @@
 class RepositoriesController < ApplicationController
   def index
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    @repos_array = JSON.parse(response.body)
+    github = GithubRepo.new({})
+    @repos_array = github.get_repos(session[:token])
   end
 
   def create
-    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    github = GithubRepo.new({})
+    github.create_repo(params[:name], session[:token])
     redirect_to '/'
   end
 end
--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -3,8 +3,30 @@ class GithubRepo
   attr_reader :name, :url
 
   def initialize(hash)
-    @name = hash["name"]
-    @url = hash["html_url"]
+    unless hash.empty?
+      @name = hash["name"]
+      @url = hash["html_url"]
+    end
   end
 
+  def get_repos(token)
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{token}", 'Accept' => 'application/json'}
+    JSON.parse(response.body)
+  end
+
+  def create_repo(name, token)
+    Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{token}", 'Accept' => 'application/json'}
+  end
+
+  def secure_access_token(github_client, github_secret, code)
+    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: github_client, client_secret: github_secret, code: code}, {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
+    access_hash["access_token"]
+  end
+
+  def set_username(token)
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{token}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    user_json["login"]
+  end
 end
